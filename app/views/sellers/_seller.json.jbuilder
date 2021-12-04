d6477db850e838653cff04590aed9415a0c56a52
Dyego Penha <dyegopenha@hotmail.com>
2021-12-04 19:02:31 -0300

    Generating Seller scaffold

---
+++ app/views/sellers/_seller.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! seller, :id, :name, :email, :phone, :address, :created_at, :updated_at
+json.url seller_url(seller, format: :json)
---
+++ db/migrate/20211204220200_create_sellers.rb
@@ -0,0 +1,12 @@
+class CreateSellers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :sellers do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
