06ece379a33079f3668e00dc2181714ee9bc3ab6
Dyego Penha <dyegopenha@hotmail.com>
2021-12-07 05:48:22 -0300

    Testando model e controller de client

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  # number, name, email, phone, address, sales
+  
+  subject {
+    described_class.new(name: "Fulano da Silva",
+                        email: "fulano@hotmail.com",
+                        phone: "81988887777",
+                        address: "Rua Fulano da Silva, 123. Boa Viagem, Recife - PE, Brasil.")
+  }
+  
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid without a email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  
+  it "is not valid without a phone" do
+    subject.phone = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a address" do
+    subject.address = nil
+    expect(subject).to_not be_valid
+  end
+
+  it '#save' do
+    cli = Client.new(name: "nome", email: "email@email.com", phone: "fone", address: "add")    
+    expect { cli.save }.to change { Client.count }.by(1)
+  end
+
+  it '#list' do
+    arr = []
+    for i in 0..5 do
+      cli = Client.new(name: "nome#{i}", email: "email#{i}@email.com", phone: "fone#{i}", address: "add#{i}")
+      cli.save
+      arr[i] = cli.id
+    end
+    clients = Client.order('created_at DESC');
+    clients.each { |obj|
+      expect(obj.id).to be > 0
+    } 
+  end 
+
+  it '#update' do
+    cli = Client.new(name: "nome", email: "email@email.com", phone: "fone", address: "add")
+    cli.save
+
+    obj = Client.find(cli.id)
+    obj.update(name: "name-upd", email: "email@email.com", phone: "fone", address: "add")
+
+    obj2 = Client.find(cli.id)
+    expect(obj2.name).to eq("name-upd")
+  end 
+
+  it '#destroy' do
+    cli = Client.new(name: "nome", email: "email@email.com", phone: "fone", address: "add")
+    cli.save
+    expect { cli.destroy }.to change { Client.count }.by(-1)
+  end 
+
 end
--- spec/requests/client_spec.rb
+++ spec/requests/client_spec.rb
@@ -1,7 +1,62 @@
 require 'rails_helper'
 
-RSpec.describe "Clients", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe ClientsController, type: :request do
+
+  #describe "GET /index" do
+  #  pending "add some examples (or delete) #{__FILE__}"
+  #end
+  subject {
+    Client.new(name: "Fulano da Silva",
+               email: "fulano@hotmail.com",
+               phone: "81988887777",
+               address: "Rua Fulano da Silva, 123. Boa Viagem, Recife - PE, Brasil.")
+  }
+
+  it 'index' do
+    get clients_path
+    expect(response).to have_http_status(:success)
   end
+
+  it '#create' do
+    get "/clients/new"
+    assert_response :success
+
+    expect {
+      post "/clients", params: { client: {name: 'Fulano', email: 'email@email.com', phone: '88887777', address: 'Rua Fulano da Silva, 123.' } }
+    }.to change { Client.count }.from(0).to(1)
+
+    assert_response :redirect
+    follow_redirect!
+    expect(response).to have_http_status(:success)
+  end 
+
+  it '#show' do
+    get clients_path(client_id: subject.id)
+    expect(response).to have_http_status(:success)
+  end 
+
+  it '#destroy' do
+    cli = Client.new(name: "nome", email: "email@email.com", phone: "fone", address: "add")    
+    cli.save
+
+    expect {
+      delete "http://localhost:3000/clients/#{cli.id}"
+    }.to change {Client.count }.from(1).to(0)
+
+    assert_redirected_to clients_path
+    follow_redirect!
+    expect(response).to have_http_status(:success)
+  end 
+
+  it '#update' do
+    cli = Client.new(name: "nome", email: "email@email.com", phone: "fone", address: "add")    
+    cli.save
+    
+    put "http://localhost:3000/clients/#{cli.id}", params: { client: {name: "Fulano Update", email: "email@email.com", phone: "88887777", address: "Rua Fulano da Silva, 123."} }
+    
+    assert_response :redirect
+    follow_redirect!
+    expect(response).to have_http_status(:success)
+  end 
+
 end
